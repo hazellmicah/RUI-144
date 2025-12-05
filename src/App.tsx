1f7edd49ec298149687e533a153f0c8ead7f0a1e
Hazellmicah <hazellmicah@gmail.com>
2025-12-05 12:45:19 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col items-center justify-center h-screen">
+        {" "}
+        {/* Container */}
+        <div className="flex">
+    
+
+          <div className="w-[700px] flex flex-col items-center">
+            {" "}
+            {/* Left Column */}
+            <h1 className="text-4xl font-bold text-violet-300">
+              Book a trip with us
+            </h1>
+            <p className="text-wrap w-150 p-2 text-purple-200">
+              Are you ready to transform your life, achieve your goals and make
+              a position change? It all starts with taking action. Embrace the
+              opportunity to seize your future now
+            </p>
+          </div>
+
+          <div className="text-center">
+            {" "}
+            {/*  Right Column*/}
+            <p className="font-bold text-2xl text-purple-950 p-2">
+              Get 40% OFF on your first trip
+            </p>
+            <h1 className="  w-80 text-purple-300 text-lg">
+              join our community and be the first to know about the latest news,
+              offers, exciting content
+            </h1>
+            <h2 className="text-2xl rounded-lg outline-2 outline-gray-300 w-78 ml-9">
+              Your email address
+            </h2>
+            <button className="p-1 w-78 h-10 text-2xl bg-red-800 rounded-lg mr-15 text-lg text-white font-bold">
+              Claim offer
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
