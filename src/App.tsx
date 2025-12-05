c8a369d3abf042608c815319f553b2889b80ef2a
Hazellmicah <hazellmicah@gmail.com>
2025-12-05 14:48:41 -0400

    Header commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,50 @@
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen">
-        {/* Container */}
-        <div className="flex">
-    
+      
+      <div className="mb-10 h-100 bg-[url(https://cdn.wallpapersafari.com/83/5/i5j73A.jpeg)] bg-top bg-cover flex items-center justify-center">
+        <h1 className="text-3xl text-white font-bold drop-shadow-lg text-center">
+          Explore More
+        </h1>
+        <h2 className="text-2xl text-white font-bold drop-shadow-lg text-center">
+          Your travel destiny, Written in the stars
+        </h2>
+      </div>
 
-          <div className="w-[700px] flex flex-col items-center">
-            {" "}
+     
+      <div className="flex flex-col items-center justify-center">
+        {/* Container */}
+
+        <div className="flex gap-6">
+          <div className="w-[700px] flex flex-col rounded-l-lg outline outline-black bg-white/5 p-4">
             {/* Left Column */}
-            <h1 className="text-4xl font-bold text-violet-300">
+            <h1 className="text-4xl font-bold text-violet-300 ">
               Book a trip with us
             </h1>
-            <p className="text-wrap w-150 p-2 text-purple-200">
+            <p className="text-wrap p-2 text-purple-200">
               Are you ready to transform your life, achieve your goals and make
               a position change? It all starts with taking action. Embrace the
               opportunity to seize your future now
             </p>
           </div>
 
-          <div className="text-center">
-            {" "}
-            {/*  Right Column*/}
-            <p className="font-bold text-2xl text-purple-950 p-2">
+          <div className="flex flex-col items-center justify-center w-90 space-y-3 p-2 rounded-r-lg outline outline-black">
+            {/* Right Column */}
+            <p className="font-bold text-2xl text-purple-950">
               Get 40% OFF on your first trip
             </p>
-            <h1 className="  w-80 text-purple-300 text-lg">
+            <h1 className="w-full text-center text-purple-300 text-lg">
               join our community and be the first to know about the latest news,
               offers, exciting content
             </h1>
-            <h2 className="text-2xl rounded-lg outline-2 outline-gray-300 w-78 ml-9">
-              Your email address
-            </h2>
-            <button className="p-1 w-78 h-10 text-2xl bg-red-800 rounded-lg mr-15 text-lg text-white font-bold">
+
+            <div className="w-full">
+              <div className="text-center text-gray-700 p-2 rounded-lg outline outline-2 outline-gray-300 w-full">
+                Your email address
+              </div>
+            </div>
+
+            <button className="w-full h-10 text-lg bg-red-800 rounded-lg text-white font-bold cursor-pointer">
               Claim offer
             </button>
           </div>
